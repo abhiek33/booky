ae7bd5d028733d9d81e427fd6cf1ed3de1a93e3e
abhiek33 <abhirlk3@gmail.com>
2025-04-15 15:00:36 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns:[
       {
-        protocol: "http",
+        protocol: "https",
         hostname: "booky-be.onrender.com",
       },
       {
